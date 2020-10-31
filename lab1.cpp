d14c2eab954d2820eb8aa1b5a55182da65905f7d
linin00 <lianjie35@outlook.com>
2020-10-31 12:55:20 +0800

    2.0

--- lab1.cpp
+++ lab1.cpp
@@ -14,8 +14,8 @@ bool lab1(int states[], int n, int button[][2], int m, bool answer[])
 
     //TODO : your solution here.
     /* assert(0); */
-    for (int i = 0; i < n; i++) {
-        if (button[i][1] <= 0 || button[i][1] >= m) {
+    for (int i = 0; i < m; i++) {
+        if (button[i][1] <= 0 || button[i][1] >= n) {
             if (states[button[i][0]] == 1) solver.addClause(mkLit(variables[button[i][0] - 1]));
             else solver.addClause(~mkLit(variables[button[i][0] - 1]));
             continue;
