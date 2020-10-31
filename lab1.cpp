f3e3e9f4ac6d4f29a1ce5b32ef810e4f0240f027
linin00 <lianjie35@outlook.com>
2020-10-31 11:48:15 +0800

    2.0

--- lab1.cpp
+++ lab1.cpp
@@ -27,9 +27,10 @@ bool lab1(int states[], int n, int button[][2], int m, bool answer[])
                     else if (states[button[i][0]] == 1 && states[button[i][1]] == 0) solver.addClause(mkLit(variables[button[i][0] - 1]), ~mkLit(variables[button[i][1] - 1]));
             continue;
         }
-        if (!button[i][0] && button[i][1]) 
+        if (!button[i][0] && button[i][1]) {
             if (states[button[i][1]] == 1) solver.addClause(mkLit(variables[button[i][1] - 1]));
             else solver.addClause(~mkLit(variables[button[i][1] - 1]));
+        }
     }
 
 
